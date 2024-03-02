eb0e6e35688021e77e22e7bbab1ba8dc4433af82
humendar <humendhar.p@gmail.com>
2024-03-02 19:37:24 +0530

    updated

--- hello-world.sh
+++ hello-world.sh
@@ -2,20 +2,20 @@
 #
 # Author         : Emertxe (https://www.emertxe.com) 
 # Date           : Tue 16 Feb 2016 16:27:07 IST
-# File           : ls_003_ch6_variables_and_expressions.sh
-# Title          : Variables and Expressions
-# Description    : A simple script to read user input and perform operations with them
-#		     
+# File           : ls_010_02_ch6_while_loop.sh
+# Title          : Loops - while
+# Description    : Sum of N natural numbers using while loop
+#
 
-echo -n "Enter number 1 : " # -n option supresses newline
-read NUM1 # Read the user input from Standard Input and store in Variable NUM1
+echo -n "Enter a number: "
+read NUM
 
-echo -n "Enter number 2 : "
-read NUM2
+let SUM=0; 
+let I=1
+while [ $I -le $NUM ]
+do
+	SUM=`expr $SUM + $I`
+	I=$((${I} + 1))
+done
 
-SUM=$(($NUM1 + $NUM2)) # Arithmetic expansion using double parentheses
-echo "The sum is $SUM"
-
-SUM=`expr $NUM1 + $NUM2` # Arithmetic expansion using backticks. 
-			#Usage of expr command to evaluate the expression
-echo "The sum is $SUM"
+echo "The sum of the first $NUM numbers is: $SUM"
