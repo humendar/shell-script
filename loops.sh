abe308b66aceb5f1d8497dfeb044f1abd3d3dee0
humendar <humendhar.p@gmail.com>
2024-03-02 22:20:39 +0530

    shell

--- loops.sh
+++ loops.sh
@@ -2,17 +2,15 @@
 #
 # Author         : Emertxe (https://www.emertxe.com) 
 # Date           : Tue 16 Feb 2016 16:27:07 IST
-# File           : ls_010_01_ch6_while_loop.sh
-# Title          : Loops - while
-# Description    : A simple script to show usage while loop
+# File           : ls_011_02_ch6_for_loop.sh
+# Title          : Loops - for
+# Description    : A simple script to demonstarte for loop [ C syntax ]
 #
 
 COUNT=0
 
-while [ $COUNT -lt 5 ]
+for ((i = 0; i < 5; i++))
 do
     echo "Loop count is ${COUNT}"
     COUNT=$((COUNT + 1))
 done
-
-echo "Done"
