3f22b00a43c3e2ade9ad676f20db7a9c564c65dd
humendar <humendhar.p@gmail.com>
2024-03-05 13:12:51 +0530

    shell practice

---
+++ delet-old-log.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  SOURCE_DIR="/temp/delet-old-log"
+  R="\e[31m"
+  G="\e[32"
+  N="\e[0m"
+if [ ! -d $SOURCE_DIR ] # ! denotes opposit
+  then
+  echo -e "$R source diretory: $SOURCE-DIR does not exist $N"
+  fi
+  FILES_TO_DELET=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+  while IFS=read -r line
+  echo "deleting file : $line"
+  done <<< $FILES_TO_DELETE 
+    
