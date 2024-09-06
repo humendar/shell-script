9f2218d28175557f7b23535e70b453329ee3ece1
humendar <humendhar.p@gmail.com>
2024-09-06 22:19:13 +0530

    shell practicing

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-  USERID=$(id-u)
-if [$USERID -ne 0]; then
+  USERID=$(id -u)
+if [$USERID: -ne 0]; then
   echo "you need to be root user to execute this script"
 exit1 
 fi
   yum install git -y
-if [$? -ne 0]; then
+if [$?: -ne 0]; then
   echo "installing git is failure"
 exit1
 else
