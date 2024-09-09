aa3ced5164967e7c46153cdbeea8ecdb3df18b11
humendar <humendhar.p@gmail.com>
2024-09-09 06:52:01 +0530

    shell practicing

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,8 +4,6 @@ b=$2
 if [ $a -ge $b ]
 then 
 echo "the variable 'a' is greater than the variable 'b'."
-else
-echo "the variable 'b' is greater than the variable 'a'."
 else 
 echo "the varible 'a' is equal to variable 'b'."
 fi 
