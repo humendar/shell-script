4dc0fb309b9eae23915ff6968c13955c9e88b669
humendar <humendhar.p@gmail.com>
2024-09-09 07:27:44 +0530

    shell practicing

---
+++ 12-functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ID=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+    echo "ERROR:: $2 --fail"
+    exit 1
+else
+echo "$2 --success"
+fi
+}
+if [ $ID -ne 0 ]
+then eho "ERROR:: please run this script with root access"
+exit 1
